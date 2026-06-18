abaedcbb298c5392b8d46de3234d69b02f840ac2
rechimonth <jonathanrechimont@gmail.com>
2026-06-18 11:23:58 -0300

    fix: configurar host 0.0.0.0 y PORT de entorno para Railway

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const PORT = Number(process.env.PORT || 3000);
+const PORT = Number(process.env.PORT || 8080);
+
 
 const ensureDbSchema = require('./middlewares/ensureDb');
 
