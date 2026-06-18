5b4fcf6530c78a2c2426af1a2d684872d5a81322
rechimonth <jonathanrechimont@gmail.com>
2026-06-18 10:22:39 -0300

    fix: habilitar SSL postgres y bind host 0.0.0.0 para Railway

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 
 const PORT = Number(process.env.PORT || 3000);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log(`MiniBlog API listening on port ${PORT}`);
 });
